9508195e4a08995985aadc74490e1bb65fc011b9
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-06-05 15:42:32 +0900

    set: 초기세팅

---
+++ JakSim/src/main/java/com/twinkle/JakSim/JakSimApplication.java
@@ -0,0 +1,13 @@
+package com.twinkle.JakSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakSimApplication.class, args);
+	}
+
+}
---
+++ JakSim/src/test/java/com/twinkle/JakSim/JakSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twinkle.JakSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
