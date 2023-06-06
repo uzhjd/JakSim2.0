fa03e16c5a48d08680ba7887afe54a17e1ccd52e
JorimJoram <wkdgyfla97@gmail.com>
2023-06-06 16:12:16 +0900

    Mustache 및 MariaDB 연결 테스트 완료

---
+++ JakSim/src/main/java/com/twinkle/JakSim/config/JDBCConfig.java
@@ -0,0 +1,45 @@
+package com.twinkle.JakSim.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+
+public class JDBCConfig {
+    @Autowired
+    private ApplicationContext application;
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource());
+
+        return tm;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSource ds = new DataSource();
+
+        ds.setDriverClassName(application.getEnvironment().getRequiredProperty("spring.datasource.driverClassName"));
+        ds.setUrl(application.getEnvironment().getRequiredProperty("spring.datasource.url"));
+        ds.setUsername(application.getEnvironment().getRequiredProperty("spring.datasource.username"));
+        ds.setPassword(application.getEnvironment().getRequiredProperty("spring.datasource.password"));
+
+        ds.setDefaultAutoCommit(true); //우리팀 유일의 희망! 대들보 같은 존재! 존재 자체가 희망 그 자체!
+        // 1조는 빛과 함께한다면... ds는 희망과 함께!
+
+        ds.setInitialSize(2);
+        ds.setMinIdle(3);
+        ds.setMaxIdle(3);
+        ds.setMaxActive(5);
+        ds.setMinEvictableIdleTimeMillis(60000);
+        ds.setTimeBetweenEvictionRunsMillis(5000);
+
+        return ds;
+    }
+}
--- JakSim/src/test/java/com/twinkle/JakSim/JakSimApplicationTests.java
+++ JakSim/src/test/java/com/twinkle/JakSim/JakSimApplicationTests.java
@@ -1,13 +1,41 @@
 package com.twinkle.JakSim;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootTest
 class JakSimApplicationTests {
+	@Autowired
+	private DataSource ds;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws SQLException {
+		Connection conn = ds.getConnection();
+		if(conn != null)
+			System.out.println("DB connection Success");
+		else
+			System.out.println("Driver Load Fail");
 	}
 
+	@Test
+	void ShowAll() throws SQLException {
+		Connection conn = ds.getConnection();
+		String sql = "select * from question where id = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, "1");
+		ResultSet rs = pstmt.executeQuery();
+		if(rs.next()) {
+			System.out.println(rs.getString("title"));
+			System.out.println(rs.getString("content"));
+		}
+		if(!conn.isClosed())
+			conn.close();
+	}
 }
