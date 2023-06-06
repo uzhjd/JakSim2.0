7b48a9df2c3ce755c9f27eded573d263ca8272d7
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-06-06 16:20:37 +0900

    set: database con

--- JakSim/src/test/java/com/twinkle/JakSim/JakSimApplicationTests.java
+++ JakSim/src/test/java/com/twinkle/JakSim/JakSimApplicationTests.java
@@ -3,11 +3,20 @@ package com.twinkle.JakSim;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootTest
 class JakSimApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws SQLException {
+		Connection conn = ds.getConnection();
+		if(conn != null)
+			System.out.println("DB connection Success");
+		else
+			System.out.println("Driver Load Fail");
 	}
 
 }
