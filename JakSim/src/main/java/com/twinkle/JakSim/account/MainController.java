65211339d93c5cf0351b1df563bb863405c2fe28
JorimJoram <wkdgyfla97@gmail.com>
2023-06-07 16:04:13 +0900

    Axios 실험 및 plugins 설치

--- JakSim/src/main/java/com/twinkle/JakSim/account/MainController.java
+++ JakSim/src/main/java/com/twinkle/JakSim/account/MainController.java
@@ -3,6 +3,9 @@ package com.twinkle.JakSim.account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -10,5 +13,11 @@ public class MainController {
     public String mainPage(Model model){
         model.addAttribute("Jaemok", "index");
         return "content/index";
+    }
+
+    @PostMapping("/index")
+    public String testPage(@RequestBody String message){
+        System.out.println(message);
+        return "redirect:/";
     }
 }
