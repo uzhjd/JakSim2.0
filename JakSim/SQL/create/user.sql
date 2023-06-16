80a75caea9f35d3b631068f739df598fe73ef8aa
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-06-17 03:19:40 +0900

    feat: SQL(user, trainer, review, payment, timetable

--- JakSim/SQL/create/user.sql
+++ JakSim/SQL/create/user.sql
@@ -20,3 +20,5 @@ INSERT INTO USER_INFO
 VALUES('wkdgyfla97', '1234', '장효림', '0', '01085945142', '0', '대구광역시', '1997-03-30', '2023-06-09 12:36:34', 1);
 INSERT INTO USER_INFO
 VALUES('hye8997', '1234', '정혜화', '1', '01012345678', '0', '전라남도 해남군', '1989-03-30', current_timestamp, 1);
+INSERT INTO USER_INFO
+VALUES('ujeong', '1004', '남유정', '1', '01012345999', '0', '서울특별시', '2000-05-28', current_timestamp, 1);
