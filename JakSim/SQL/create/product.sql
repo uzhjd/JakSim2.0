3c6a9561ec18e86f25cb0c58b97f6ef59ab59e84
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-07-31 15:38:59 +0900

    data: product Data set

--- JakSim/SQL/create/product.sql
+++ JakSim/SQL/create/product.sql
@@ -14,8 +14,58 @@ CREATE TABLE PRODUCT(
 COMMIT;
 
 INSERT INTO PRODUCT
-VALUES(NULL, 'wkdgyfla97', '바디프로필 3개월 패키지(개인)', 600000, 0, 90, 24);
+VALUES(NULL, 'angel1', '바디 프로필 1개월 패키지(개인)', 560000, 1, 30, 8);
 INSERT INTO PRODUCT
-VALUES(NULL, 'hye8997', '생활근력 만들기(단체)', 400000, 0, 60, 24);
+VALUES(NULL, 'angel1', '바디 프로필 1개월 패키지(단체)', 480000, 2, 30, 12);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel1', '바디 프로필 3개월 패키지(개인)', 840000, 1, 90, 24);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel1', '바디 프로필 3개월 패키지(단체)', 756000, 2, 90, 36);
+
+VALUES(NULL, 'angel1', '생활 근력 1개월 패키지(개인)', 500000, 1, 30, 8);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel1', '생활 근력 1개월 패키지(단체)', 400000, 2, 30, 12);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel1', '생활 근력 3개월 패키지(개인)', 800000, 1, 90, 24);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel1', '생활 근력 3개월 패키지(단체)', 700000, 2, 90, 36);
+
+
+
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '체력 강화 1개월 패키지(개인)', 400000, 1, 30, 8);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '체력 강화 1개월 패키지(단체)', 320000, 2, 30, 12);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '체력 강화 3개월 패키지(개인)', 700000, 1, 90, 24);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '체력 강화 3개월 패키지(단체)', 616000, 2, 90, 36);
+
+VALUES(NULL, 'angel2', '생활 근력 1개월 패키지(개인)', 400000, 1, 30, 8);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '생활 근력 1개월 패키지(단체)', 300000, 2, 30, 12);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '생활 근력 3개월 패키지(개인)', 700000, 1, 90, 24);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel2', '생활근력 3개월 패키지(단체)', 600000, 2, 90, 36);
+
+
+
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '자세 교정 1개월 패키지(개인)', 400000, 1, 30, 4);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '체력 강화 1개월 패키지(단체)', 400000, 2, 30, 8);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '체력 강화 3개월 패키지(개인)', 840000, 1, 90, 12);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '체력 강화 3개월 패키지(단체)', 840000, 2, 90, 24);
+
+VALUES(NULL, 'angel3', '생활 근력 1개월 패키지(개인)', 560000, 1, 30, 8);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '생활 근력 1개월 패키지(단체)', 600000, 2, 30, 12);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '생활 근력 3개월 패키지(개인)', 908000, 1, 90, 24);
+INSERT INTO PRODUCT
+VALUES(NULL, 'angel3', '생활 근력 3개월 패키지(단체)', 980000, 2, 90, 36);
 
 COMMIT;
